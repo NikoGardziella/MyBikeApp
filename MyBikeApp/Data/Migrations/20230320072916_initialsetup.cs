ebd074cf7e49cc80f435e499a9e9b10c7f958fd9
NikoGardziella <gardziella@gmail.com>
2023-03-20 10:32:21 +0200

    Add project files.

---
+++ MyBikeApp/Data/Migrations/20230320072916_initialsetup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBikeApp.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Station",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartStations = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EndStation = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Station", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Station");
+        }
+    }
+}
---
+++ MyBikeApp/Models/Station.cs
@@ -0,0 +1,14 @@
+﻿namespace MyBikeApp.Models
+{
+	public class Station
+	{
+        public int Id { get; set; }
+        public string StartStations { get; set; }
+        public string EndStation { get; set; }
+
+        public Station()
+        {
+
+        }
+    }
+}
